72dd9a2cfc80631323a5047648edaf42961d883a
Josh Burgess <joshburgesswebdev@gmail.com>
2019-08-31 08:22:37 -0400

    add @since version comments to main index

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,17 @@ import * as cont from './Cont'
 import * as contT from './ContT'
 import * as monadCont from './MonadCont'
 
-export { cont, contT, monadCont }
+export {
+  /**
+   * @since 0.0.1
+   */
+  cont,
+  /**
+   * @since 0.0.1
+   */
+  contT,
+  /**
+   * @since 0.0.1
+   */
+  monadCont,
+}
